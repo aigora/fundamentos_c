df3a9c79bdc0f7f9c94a66e6988e0a6a06e9c1fd
lucasgomezmontejano <38069882+lucasgomezmontejano@users.noreply.github.com>
2019-02-20 13:02:16 +0100

    Calculo del Metabolismo basal en un hombre

---
+++ ejercicio TMB.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	
+	float peso,altura;
+	int edad,TMB;
+	
+	printf("Vamos a calcular el metabolismo basal de un hombre.\n");
+	printf("Indique su peso en kg:\n");scanf("%f",&peso);
+	printf("Indique su altura en cm:\n");scanf("%f",&altura);
+	printf("Indique su edad:\n");scanf("%i",&edad);
+	
+	TMB = (10*peso)+(6.25*altura)-(5*edad)+5;
+
+	printf("Tu metabolismo basal es : %i kcal",TMB);
+	
+	return 0;
+}
+

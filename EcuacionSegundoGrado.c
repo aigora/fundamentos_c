79c1fb50695043aea57576982976fef20b82aadc
Diego105 <38649561+Diego105@users.noreply.github.com>
2019-02-20 13:02:41 +0100

    Add files via upload
    Resolver ecuaciones de segundo grado

---
+++ EcuacionSegundoGrado.c
@@ -0,0 +1,35 @@
+/*
+Autor: Diego Medina
+Grupo:103
+Descaripción: Este programa resuelve una ecuación de segundo grado a partir de introducir los terminos a,b y c.
+*/
+#include <stdio.h>
+#include <math.h>
+
+int main()
+
+{
+	float a,b,c,resultado1,resultado2;
+	
+	printf("*RESOLVER ECUACIONES DE SEGUNDO GRADO*\n");
+	printf("Escribe el termino a\n");
+scanf("%f",&a);
+
+	printf("Ahora escribe el termino b\n");
+	
+scanf("%f", &b);
+
+	printf("Finalmente pon el termino c\n");
+	
+scanf("%f", &c);
+
+
+resultado1= (-b+(sqrt(b*b-4*a*c)))/2*a;      //raiz positiva//
+resultado2= (-b-(sqrt(b*b-4*a*c)))/2*a;     //raiz negativa//
+
+
+	printf("x1= %.1f\n", resultado1);
+	printf("x2= %.1f\n", resultado2);
+
+
+}

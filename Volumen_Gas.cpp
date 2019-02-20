58853e07a94182700f215b0e2baa0069fb597abd
CruzBojana18 <47431817+CruzBojana18@users.noreply.github.com>
2019-02-20 13:00:12 +0100

    Volumen que ocupa un gas

---
+++ Volumen_Gas.cpp
@@ -0,0 +1,26 @@
+/*
+Autor: Carlos Cruz
+Grupo: Q103
+Descripción: Programa que calcula el volumen que ocupa un gas
+*/
+
+#include <stdio.h>
+
+int main () {
+	float moles, temperatura, presion, volumen;
+	
+	printf("CALCULO DEL VOLUMEN QUE OCUPA UN GAS\n");
+	
+	printf("Introduce la cantidad de moles del gas:\n");
+	scanf("%f", &moles);
+	printf("Introduce la temperatura en grados Kelvin:\n");
+	scanf("%f", &temperatura);
+	printf("Introduce la presion en atmosferas:\n");
+	scanf("%f", &presion);
+	
+	volumen=moles*temperatura*8.206E-5/presion;
+	
+	printf("El volumen que ocupa el gas es: %.5f metros cubicos", volumen);
+	
+	return 0;
+}

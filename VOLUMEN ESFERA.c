a05b79c0819f642f345e7fc33d9c6413a1ef49ad
SamuelHornsbyAltares <47475011+SamuelHornsbyAltares@users.noreply.github.com>
2019-02-20 12:58:48 +0100

    CalcularVolumenEsfera

---
+++ VOLUMEN ESFERA.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+const float PI=3.141592;
+printf("BIENVENIDO A LA CALCULADORA DE VOLUMENES PARA ESFERAS\n");
+system("PAUSE\n");
+float r,v;
+printf("Introduce el radio de la esfera en metros:\n");
+scanf("%f",&r);
+v=(4/3)*PI*r*r*r;
+printf("El volumen de su esfera es el siguiente:%.2f m3\n",v);
+system("PAUSE\n");
+return 0;	
+}

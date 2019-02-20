d0f7b0e9fa12429cd8636a2442219cc49cc14b18
pedrogonzalezlozoya <38070032+pedrogonzalezlozoya@users.noreply.github.com>
2019-02-20 12:59:21 +0100

    Add files via upload

---
+++ metabolismo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+float peso,altura,TMB;
+int edad;
+printf("Vamos a calcular el metabolismo basal de una mujer.\n");
+printf("Indique su peso (Kg):");scanf("%f",&peso);
+printf("Indique su altura (cm):");scanf("%f",&altura);
+printf("Indique su edad:");scanf("%i",&edad);
+
+TMB = (10*peso)+(6.25*altura)-(5*edad)-161;
+
+printf("Su metabolismo basal es (TMB):%.2f",TMB);
+return 0;
+}
+

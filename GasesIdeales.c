f1f0566b8c1f9a67ec68269b1f5e6a592357f0f5
Marina <47520579+marinapolov@users.noreply.github.com>
2019-02-21 20:49:16 +0100

    Add files via upload

---
+++ GasesIdeales.c
@@ -0,0 +1,20 @@
+//Marina Polo 54809 Q103
+
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+    float presion, volumen, moles, R, temperatura;
+    printf("Indicar numero de moles");
+    scanf("%f",&moles);
+    
+    printf("Introducir constante de los gases ideales"); //expresar en atm*L/K*mol
+    scanf("%f",&R);
+    
+    printf("Proponer una temperatura en grados Kelvin"); scanf("%f",&temperatura);
+    printf("Indicar volumen del recipiente expresado en L"); scanf("%f",&volumen);
+    presion=((moles*R*temperatura)/(volumen));
+    
+    printf("La presion segun los datos obtenidos es;%f",presion);
+    return 0;
+}
